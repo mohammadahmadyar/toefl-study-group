f35e7f0aa7d605e71ccca209fb2dae23a657543b
Mohammad Ahmadi <89262653+mohammadahmadyar@users.noreply.github.com>
2026-07-10 19:21:50 +0430

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,42 @@
+import {
+    db
+} from "./firebase.js";
+
+
+import {
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+
+const examList = document.getElementById("examList");
+
+
+const examsSnapshot = await getDocs(
+    collection(db,"exams")
+);
+
+
+
+examsSnapshot.forEach((doc)=>{
+
+
+    const exam = doc.data();
+
+
+    examList.innerHTML += `
+
+    <div>
+
+        <h3>${exam.title}</h3>
+
+        <button>
+        Start Test
+        </button>
+
+    </div>
+
+    `;
+
+
+});
